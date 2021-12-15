be5f785bc8f799269e9d2b9ed87db7264bac9166
Edclydson <edclydson.sousa@gmail.com>
2021-12-15 11:43:11 -0300

    ATV

---
+++ ATV2EJP.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ATV2EJP 
+{   
+    // PROGRAMA 2
+    /* Criar um programa onde sejam informados a base
+        e altura  de um retangulo  e o programa  calcula e mostra
+        a sua area*/
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite a altura do retangulo:");
+        int altura = scan.nextInt();
+        System.out.println("Digite a base do retangulo:");
+        int base = scan.nextInt();
+        System.out.println("A área do retangulo é: "+base * altura);   
+    }    
+}
---
+++ ATV3EJP.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ATV3EJP 
+{
+    // PROGRAMA 3
+    /*Criar um programa onde seja digitado o ano atual,
+     o ano que a pessoa nasceu e o programa calcula e 
+     mostra  a sua idade atual */
+    public static void main(String[] args) 
+    {
+         Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o ano atual:");
+        int ano_atual = scan.nextInt();
+        System.out.println("Digite o ano de nascimento:");
+        int ano_nascimento = scan.nextInt();
+        System.out.println("A sua idade é: "+ (ano_atual-ano_nascimento) +" anos");
+    }    
+}
