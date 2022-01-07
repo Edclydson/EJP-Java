e0489a2045e0d4f0ab92ab31363c20ea62abe202
Edclydson <edclydson.sousa@gmail.com>
2022-01-06 21:11:23 -0300

    DESAFIO/V3.9

--- DESAFIO_1_EJP_POKEMON/Pokemon_PACKAGE/MundoPokemon_PACKAGE/MundoAberto.java
+++ DESAFIO_1_EJP_POKEMON/Pokemon_PACKAGE/MundoPokemon_PACKAGE/MundoAberto.java
@@ -1,5 +1,7 @@
 package DESAFIO_1_EJP_POKEMON.Pokemon_PACKAGE.MundoPokemon_PACKAGE;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class MundoAberto 
@@ -7,6 +9,25 @@ public class MundoAberto
     public static void explorar()
     {
         JOptionPane.showMessageDialog(null,"Bem vindo ao Mundo Aberto de Pokemon!\nNo Mundo Aberto você encontra novos \nPokemons e consegue encontrar \nmais Pokebolas");
+        
+        Random rand = new Random();
+        int z;
+        while(true)
+        {
+            z = rand.nextInt(1,50);
+            if(z<= 25)
+            {
+                System.out.println("Andando...");
+            }
+            else if(z>=26 && z <= 40)
+            {
+                JOptionPane.showMessageDialog(null,"Você foi atacado por um pokemon selvagem!");
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,"Você encontrou uma pokebola!");
+            }
+        }
     }
     
 }
