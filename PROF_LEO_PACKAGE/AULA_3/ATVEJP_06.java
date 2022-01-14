1073dfefa5d6f3d39d186312bc2a1abb329f88c8
Edclydson <edclydson.sousa@gmail.com>
2022-01-13 21:13:12 -0300

    ATV

---
+++ PROF_LEO_PACKAGE/AULA_3/ATVEJP_06.java
@@ -0,0 +1,17 @@
+package PROF_LEO_PACKAGE.AULA_3;
+
+public class ATVEJP_06 
+{
+public static void main(String[] args) 
+    {
+        int idade = 20;
+        if(idade>=18)
+        {
+            System.out.println("Você é maior de idade!");
+        }
+        else
+        {
+            System.out.println("Você é menor de idade!");
+        }
+    }    
+}
