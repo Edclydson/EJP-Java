d314a8a1156eb044292172c99e510698258ba198
Edclydson <edclydson.sousa@gmail.com>
2022-01-13 21:21:25 -0300

    ATV

---
+++ PROF_LEO_PACKAGE/AULA_4/ATVEJP_07.java
@@ -0,0 +1,24 @@
+package PROF_LEO_PACKAGE.AULA_4;
+
+import javax.swing.JOptionPane;
+
+public class ATVEJP_07 
+{
+    public static void main(String[] args) 
+    {
+        String login = "admin";
+        String senha = "1234";
+
+        String l = JOptionPane.showInputDialog(null,"Login:");
+        String s = JOptionPane.showInputDialog(null,"Senha:");
+
+        if(l.equals(login) && s.equals(senha))
+        {
+            JOptionPane.showMessageDialog(null,"Acesso Liberado!");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"Acesso Negado!");
+        }
+    }    
+}
