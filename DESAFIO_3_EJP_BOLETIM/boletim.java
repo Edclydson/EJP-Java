fa45e14a99fe0497d47e16a1478481489efb6ef2
Edclydson <edclydson.sousa@gmail.com>
2022-01-19 21:18:49 -0300

    DESAFIO

---
+++ DESAFIO_3_EJP_BOLETIM/boletim.java
@@ -0,0 +1,18 @@
+package DESAFIO_3_EJP_BOLETIM;
+
+public class boletim {
+    public static void main(String[] args) 
+    {
+        estudante aluno = new estudante();
+        aluno.setNome("Edclydson");
+        aluno.setNota1(5.5);
+        aluno.setNota2(7.0);
+        aluno.setNota3(9.5);
+        aluno.setNota4(6.0);
+        aluno.setSigno("Aquário");
+        aluno.mostraboletim();
+    }
+
+    
+    
+}
---
+++ DESAFIO_3_EJP_BOLETIM/estudante.java
@@ -0,0 +1,65 @@
+package DESAFIO_3_EJP_BOLETIM;
+
+public class estudante 
+{
+
+    private String nome;
+    private String signo;
+    private double nota1,nota2,nota3,nota4;
+    private double media;
+    public void mostrarmedia()
+    {
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A media do aluno "+nome+" é: "+media);
+    }
+     public void mostrasigno(){System.out.println("O signo do aluno "+nome+" é: "+signo);}
+    public void mostraboletim(){
+        System.out.println("Nota 1:"+nota1+"\nNota 2:"+nota2+"\nNota 3:"+nota3+"\nNota 4:"+nota4);
+        mostrarmedia();
+        mostrasigno();
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSigno() {
+        return signo;
+    }
+    public void setSigno(String signo) {
+        this.signo = signo;
+    }
+    public double getNota1() {
+        return nota1;
+    }
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+    public double getNota2() {
+        return nota2;
+    }
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+    public double getNota3() {
+        return nota3;
+    }
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+    public double getNota4() {
+        return nota4;
+    }
+    public void setNota4(double nota4) {
+        this.nota4 = nota4;
+    }
+    public double getMedia() {
+        return media;
+    }
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    
+}
